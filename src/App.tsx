f255dc82307ddcdc0e6f3d227bef4fd7421dfbff
vivekk-snippyly <vivek@snippyly.com>
2022-03-09 11:00:27 +0530

    Minor changes.

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ function App() {
   }, [])
 
   const init = async () => {
-    const snippyly = await Snippyly.init('TA66fUfxZVtGBqGxSTCz', {
+    const client = await Snippyly.init('TA66fUfxZVtGBqGxSTCz', {
       featureAllowList: [], // To allow specific features only
       // userIdAllowList: ['abcd'], // To allow specific users only
       urlAllowList: [], // To allow snippyly in specific screens only
     }); // Add your Api Key here
-    console.log('snippyly', snippyly);
-    setClient(snippyly);
+    console.log('snippyly client', client);
+    setClient(client);
   }
 
   return (
