1d560e8368178496e8f597cc510c8d63b9649e67
vivek <vivek@snippyly.com>
2022-12-08 02:01:26 +0530

    Updated snippyly to 1.0.62. Integrated @snippyly/react library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,38 @@
-import { Snippyly } from '@snippyly/sdk';
-import React, { useEffect, useState } from 'react';
+import { SnippylyComments, SnippylyCommentsSidebar, SnippylyCommentTool, SnippylyCursor, SnippylyProvider } from '@snippyly/react';
+import { Snippyly } from '@snippyly/types';
+import { useState } from 'react';
 import './App.css';
 import Tabs from './components/Tabs/Tabs';
 import Toolbar from './components/Toolbar/Toolbar';
-import { SnippylyContext } from './context/SnippylyContext';
 
 function App() {
-  const [client, setClient] = useState<Snippyly>(null as any);
 
   const [selectedMenu, setSelectedMenu] = useState();
 
-  useEffect(() => {
-    init();
-  }, [])
-
-  const init = async () => {
-    const client = await Snippyly.init('TA66fUfxZVtGBqGxSTCz', {
-      featureAllowList: [], // To allow specific features only
-      // userIdAllowList: ['abcd'], // To allow specific users only
-      urlAllowList: [], // To allow snippyly in specific screens only
-    }); // Add your Api Key here
-    console.log('snippyly client', client);
-    setClient(client);
-
-    // Enable attachment feature
-    const commentElement = client.getCommentElement();
-    commentElement.enableAttachment(true);
-    commentElement.showScreenSizeInfo(true);
+  const init = async (client?: Snippyly) => {
+    if (client) {
+      // Enable attachment feature
+      const commentElement = client.getCommentElement();
+      commentElement.enableAttachment(true);
+      commentElement.showScreenSizeInfo(true);
+    }
   }
 
   return (
-    <SnippylyContext.Provider value={{ client }}>
+    <SnippylyProvider apiKey='TA66fUfxZVtGBqGxSTCz' config={{
+      featureAllowList: [], // To allow specific features only
+      // userIdAllowList: ['abcd'], // To allow specific users only
+      urlAllowList: [], // To allow snippyly in specific screens only
+    }} onClientLoad={(client) => init(client)}>
       <div>
-        <snippyly-cursor></snippyly-cursor>
-        <snippyly-comments></snippyly-comments>
-        <snippyly-comments-sidebar></snippyly-comments-sidebar>
-        <snippyly-comment-tool></snippyly-comment-tool>
+        <SnippylyCursor />
+        <SnippylyComments />
+        <SnippylyCommentsSidebar />
+        <SnippylyCommentTool />
         <Toolbar onMenuSelect={(menu: any) => setSelectedMenu(menu)} />
         <Tabs selectedMenu={selectedMenu} />
       </div>
-    </SnippylyContext.Provider>
+    </SnippylyProvider>
   );
 }
 
